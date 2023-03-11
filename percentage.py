88ed3679de0b2550562f4f33f08136714eee69d6
Didula Induwara <didulainduwaraamarasinghe@gmail.com>
2023-03-12 00:25:24 +0530

    test code using unicodes

--- percentage.py
+++ percentage.py
@@ -1,16 +1,35 @@
 import psutil
 from plyer import notification
-# import numpy as np
+import numpy as np
+
+arr = np.array([[1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0]])
+
 
 b = psutil.sensors_battery()
 
 print(b.percent)
 print(b.power_plugged)
 
+msg = ''
 
+for i in range(6):
+    for j in range(5):
+        if(arr[i][j] == 1):
+            msg += '\u2591'
+        else:
+            msg += ' '
+    msg += '\n'
+print(msg)
+print('\033[0m')
 notification.notify(
-    title = 'Battery',
-    message = str(b.percent) + ' ' + str(b.power_plugged),
+    title = 'test',
+    message = msg,
     app_icon = "image1.ico",
     timeout = 10,
 )
+# print("\u2502  \u2591")
