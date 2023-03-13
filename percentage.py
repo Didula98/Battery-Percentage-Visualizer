e5891133346e70765f913e0e5ae06248a0babd77
Didula Induwara <didulainduwaraamarasinghe@gmail.com>
2023-03-13 14:48:49 +0530

    Print battery with plugged-in conditions

--- percentage.py
+++ percentage.py
@@ -1,66 +1,79 @@
 import psutil
 from plyer import notification
 import numpy as np
-
+HEIGHT = 5 #shoud be odd
+WIDTH = 20
 
 
 b = psutil.sensors_battery()
 percentage = b.percent
+isPlugged = b.power_plugged
 print(b.percent)
 print(b.power_plugged)
+print((percentage*18)//100)
+battery = ''
 
-msg = ''
-
-for i in range(5):
-    fillArea = (percentage*18)//100
+for i in range(HEIGHT):
+    fillArea = (percentage*(WIDTH-2))//100
     # fillArea = 3
-    for j in range(20):
+    for j in range(WIDTH):
         if i == 0:
             if j == 0:
-                msg +=  "\u250C"
+                battery +=  "\u250C"
        
-            elif j == 19:
-                msg += "\u2510"
+            elif j == WIDTH-1:
+                battery += "\u2510"
              
             else:
-                msg += "\u2500"
+                battery += "\u2500"
 
-        elif i == 4:
+        elif i == HEIGHT-1:
             if j == 0:
-                msg +=  "\u2514"
+                battery +=  "\u2514"
        
-            elif j == 19:
-                msg += "\u2518"
+            elif j == WIDTH-1:
+                battery += "\u2518"
              
             else:
-                msg += "\u2500"
+                battery += "\u2500"
      
         else:
-            if j == 19:
-                if i == 1:
-                   msg += "\u2514\u2510"
-                elif i == 3:
-                    msg += "\u250C\u2518"
+            if j == WIDTH-1:
+                if i == (HEIGHT//2)-1:
+                   battery += "\u2514\u2510"
+                elif i == (HEIGHT//2)+1:
+                    battery += "\u250C\u2518"
+                elif i == (HEIGHT//2):
+                    if isPlugged:
+                        battery += " \u2502\u26A1"
+                    else:
+                        battery += " \u2502"
                 else:
-                    msg += " \u2502"
+                    battery += "\u2502"
             elif j == 0:
-                msg += "\u2502"
+                battery += "\u2502"
             else:
-   
+                
                 if fillArea != 0:
-                    msg += "\u2591"
+                    if isPlugged:
+                        battery += "\033[42m "
+                    else:
+                        battery += "\033[47m "
                     fillArea -= 1
                 else:
-                    msg += " "
+                    battery += "\033[0m "
                     
 
         
-    msg += "\n"
+    battery += "\n"
 
-print(msg)
+print(battery)
+print("\033[0m")
+
+print((7//2)-1)
 # notification.notify(
 #     title = 'test',
-#     message = msg,
+#     message = battery,
 #     app_icon = "image1.ico",
 #     timeout = 10,
 # )
@@ -69,4 +82,4 @@ print(msg)
 # print("\u2500\u2510")
 
 # print("\u2510\n\u2518")
-# print(a)
+# print("\u26A1")
